0adfca185eb63de660dc1bf7f3721963de0413ad
LordBaibus <jairusmaniago0204@gmail.com>
2026-02-21 08:07:02 +0800

    ﻿Merge branch 'master' of https://github.com/The-Kolokoys/MIDTERM # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,21 @@
-import Flutter
 import UIKit
+import Flutter
 import GoogleMaps
-
-GMSServices.provideAPIKey("AIzaSyBujtiCvaPNc8cTkh7cDH7j4XxE2MvbrvA")
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+
+        // Provide your Google Maps API Key here
+        GMSServices.provideAPIKey("AIzaSyBujtiCvaPNc8cTkh7cDH7j4XxE2MvbrvA")
+
+        // Register Flutter plugins
+        GeneratedPluginRegistrant.register(with: self)
+
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
